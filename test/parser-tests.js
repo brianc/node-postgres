a921d6c144e80b3c87cc7813231d768eec4ba01a
brianc <brian.m.carlson@gmail.com>
2010-09-29 00:12:04 -0500

    made Parser non-static

--- test/parser-tests.js
+++ test/parser-tests.js
@@ -1,7 +1,7 @@
 require(__dirname+'/test-helper');
 test('Parser on single messages', function() {
   test('parses AuthenticationOk message', function() {
-    var result = Parser.parse(Buffer([0x52, 00, 00, 00, 08, 00, 00, 00, 00]));
+    var result = new Parser().parse(Buffer([0x52, 00, 00, 00, 08, 00, 00, 00, 00]));
     assert.equal(result.name, 'AuthenticationOk');
     assert.equal(result.id, 'R');
     assert.equal(result.length, 8);
@@ -11,7 +11,7 @@ test('Parser on single messages', function() {
     var firstString = [0x63, 0x6c, 0x69, 0x65, 0x6e, 0x74, 0x5f, 0x65, 0x6e, 0x63, 0x6f, 0x64, 0x69, 0x6e, 0x67, 0];
     var secondString = [0x55, 0x54, 0x46, 0x38, 0];
     var bytes = [0x53, 0, 0, 0, 0x19].concat(firstString).concat(secondString);
-    var result = Parser.parse(Buffer(bytes));
+    var result = new Parser().parse(Buffer(bytes));
     assert.equal(result.name, 'ParameterStatus');
     assert.equal(result.id, 'S');
     assert.equal(result.length, 25);
