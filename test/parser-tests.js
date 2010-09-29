0ca7b93bb0f5f5a1471c34b1e6e7c771d569a18c
brianc <brian.m.carlson@gmail.com>
2010-09-28 23:31:40 -0500

    added test for parameterstatus message

--- test/parser-tests.js
+++ test/parser-tests.js
@@ -7,6 +7,12 @@ test('Parser on single messages', function() {
     assert.equal(result.length, 8);
   });
 
+  test('parses ParameterStatus message', function() {
+    var firstString = [0x63, 0x6c, 0x69, 0x65, 0x6e, 0x74, 0x5f, 0x65, 0x6e, 0x63, 0x6f, 0x64, 0x69, 0x6e, 0x67, 0];
+    var secondString = [0x55, 0x54, 0x46, 0x38, 0];
+    var bytes = [53, 0, 0, 0, 0x19].concat(firstString).concat(secondString);
+    var result = Parser.parse(Buffer(bytes));
 
+  });
 });
 
