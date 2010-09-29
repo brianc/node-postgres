dc427a992305e98d2497d5f081918b52ca10ccc0
brianc <brian.m.carlson@gmail.com>
2010-09-28 23:34:10 -0500

    use hex code in message buffer simulation

--- test/parser-tests.js
+++ test/parser-tests.js
@@ -10,7 +10,7 @@ test('Parser on single messages', function() {
   test('parses ParameterStatus message', function() {
     var firstString = [0x63, 0x6c, 0x69, 0x65, 0x6e, 0x74, 0x5f, 0x65, 0x6e, 0x63, 0x6f, 0x64, 0x69, 0x6e, 0x67, 0];
     var secondString = [0x55, 0x54, 0x46, 0x38, 0];
-    var bytes = [53, 0, 0, 0, 0x19].concat(firstString).concat(secondString);
+    var bytes = [0x53, 0, 0, 0, 0x19].concat(firstString).concat(secondString);
     var result = Parser.parse(Buffer(bytes));
 
   });
