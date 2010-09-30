d852f7f1c40ffdf822cfefd63a3079927976c52a
brianc <brian.m.carlson@gmail.com>
2010-09-30 00:36:11 -0500

    updated watch file

--- watch.rb
+++ watch.rb
@@ -1,12 +1,16 @@
 #watch file
 
-watch('lib/(.*)\.js') { |md|
-  system('echo "lib changed"')
-}
-
-watch('test/(.*)\.js') { |md|
-  system("node #{md}")
+def run_parser_tests
+  system("node test/parser-tests.js")
   puts ""
   puts("waiting...")
   puts ""
+end
+
+watch('lib/(.*)\.js') { |md|
+  run_parser_tests
+}
+
+watch('test/(.*)\.js') { |md|
+  run_parser_tests
 }
