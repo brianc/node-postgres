e69b6faa82cefea6ca267adaf7ac399e5b2717bc
brianc <brian.m.carlson@gmail.com>
2010-10-10 18:20:24 -0500

    added null terminator (c-string) to query

--- test/api-tests.js
+++ test/api-tests.js
@@ -3,10 +3,14 @@ require(__dirname+"/test-helper");
 //little helper to make a
 //fresh client to the test DB
 var makeClient = function() {
-  return new Client({
+  var client = new Client({
     user: 'brian',
     database: 'pgjstest'
   });
+  client.on('Error', function(msg) {
+    console.log(msg);
+  });
+  return client;
 };
 
 var client1 = makeClient();
@@ -27,12 +31,13 @@ var client3 = makeClient();
 client3.connect();
 client3.on('ReadyForQuery', function() {
   console.log('client3 ready for query');
-  var query = client3.query('create temporary table bang (id integer)');
-  query.on('end', function() {
-    client3.disconnect();
-  });
 
 });
+var query = client3.query('create temporary table bang (id integer)');
+query.on('end', function() {
+  client3.disconnect();
+});
+
 
 // client.query('create temporary table bang (id integer)');
 // client.query('insert into bang(id) VALUES(1)');
