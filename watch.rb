c511997c4d78267b96aba21234c476b946c4109c
brianc <brian.m.carlson@gmail.com>
2010-10-10 22:27:42 -0500

    updated watch file to use node test script

--- watch.rb
+++ watch.rb
@@ -1,13 +1,8 @@
 #watch file
-def run_test_file(f)
-  puts "running #{f}"
-  system "node #{f}"
-  puts "done"
-end
 watch('lib/(.*)\.js') { |md|
-  puts Dir["test/*.js"].each { |f| run_test_file(f) }
+  system "node test-units.js"
 }
 
 watch('test/(.*)\.js') { |md|
-  run_test_file(md)
+  system "node test-units.js"
 }
