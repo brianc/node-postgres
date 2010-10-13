b1d027c6d15182143dd8d61bcbfed54f18f30175
brianc <brian.m.carlson@gmail.com>
2010-10-12 21:10:44 -0500

    added script to push out db types

---
+++ script/list-db-types.js
@@ -0,0 +1,14 @@
+var Client = require(__dirname+"/../lib/client");
+var client = new Client({
+  user: 'brian',
+  database: 'postgres'
+});
+client.connect();
+var query = client.query('select oid, typname, typlen from pg_type order by typname');
+query.on('row', function(row) {
+  console.log(row);
+});
+query.on('end',function() {
+  client.disconnect();
+})
+
