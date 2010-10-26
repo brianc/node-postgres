2a594b624d40d1c5cf02999f30f625038fbe69db
brianc <brian.m.carlson@gmail.com>
2010-10-25 19:19:01 -0500

    remove redundant auth tests and connect with command line arguments

--- test/integration/connection/no-password-tests.js
+++
@@ -1,8 +0,0 @@
-var helper = require(__dirname+'/test-helper');
-test('can log in with no password', function() {
-  helper.authConnect(function(con) {
-    assert.raises(con, 'readyForQuery', function() {
-      con.end();
-    });
-  });
-});
