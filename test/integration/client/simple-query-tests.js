490bfdaebde051dda68b48b98594596630b5d5aa
brianc <brian.m.carlson@gmail.com>
2010-10-26 00:31:58 -0500

    slight refactor to client integration tests in prep for deeper testing effort

--- test/integration/client/simple-query-tests.js
+++ test/integration/client/simple-query-tests.js
@@ -2,7 +2,7 @@ var helper = require(__dirname+"/test-helper");
 //before running this test make sure you run the script create-test-tables
 test("selects rows", function() {
   var client = helper.client();
-  client.connect();
+
   var query = client.query("select * from person");
   var rowCount = 0;
   query.on('row', function(row) {
