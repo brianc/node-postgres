30246c16d52a499823de26adb79cad5f3eb845fa
brianc <brian.m.carlson@gmail.com>
2010-10-27 18:49:56 -0500

    script for debugging of text version of type output

---
+++ script/dump-db-types.js
@@ -0,0 +1,23 @@
+var pg = require(__dirname + '/../lib');
+var args = require(__dirname + '/../test/cli');
+
+var queries = [
+  "select CURRENT_TIMESTAMP", 
+  "select interval '1 day' + interval '1 hour'",
+  "select TIMESTAMP 'today'"];
+
+queries.forEach(function(query) {
+
+  var client = new pg.Client({
+    user: args.user,
+    database: args.database,
+    password: args.password
+  });
+  client.connect();
+  client
+    .query(query)
+    .on('row', function(row) {
+      console.log(row);
+      client.end();
+    });
+});
