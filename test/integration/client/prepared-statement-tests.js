0787df4f746282d761985dae87882b8ee8db00c8
brianc <brian.m.carlson@gmail.com>
2010-10-28 01:04:55 -0500

    remove detailed logging from integration test

--- test/integration/client/prepared-statement-tests.js
+++ test/integration/client/prepared-statement-tests.js
@@ -2,9 +2,7 @@ var helper = require(__dirname +'/test-helper');
 
 test("simple prepared statement", function(){
   var client = helper.client();
-  client.connection.on('message', function(msg) {
-    console.log(msg.name);
-  });
+
   var query = client.query({
     text: 'select age from person where name = $1',
     values: ['Brian']
