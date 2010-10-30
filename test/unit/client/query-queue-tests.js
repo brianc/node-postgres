577f48f824be23d214eb3646b228643dd0fd6715
bmc <bmc@localhost.(none)>
2010-10-29 20:48:31 -0400

    added 'drain' event to client which fires after all pending queries are done

---
+++ test/unit/client/query-queue-tests.js
@@ -0,0 +1,49 @@
+var helper = require(__dirname + '/test-helper');
+var con = new Connection({stream: "NO"});
+var client = new Client({connection:con});
+
+
+con.connect = function() {
+  con.emit('connect');
+};
+con.query = function() {
+};
+client.connect();
+
+var raisedDrain = false;
+client.on('drain', function() {
+  raisedDrain = true;
+});
+
+client.query("hello");
+client.query("sup");
+client.query('boom');
+
+test("with pending queries", function() {
+  test("does not emit drain", function() {
+    assert.equal(raisedDrain, false);
+  });
+});
+
+test("after some queries executed", function() {
+  con.emit('readyForQuery');
+  test("does not emit drain", function() {
+    assert.equal(raisedDrain, false);
+  });
+});
+
+test("when all queries are sent", function() {
+  con.emit('readyForQuery');
+  con.emit('readyForQuery');
+  test("does not emit drain", function() {
+    assert.equal(raisedDrain, false);
+  });
+});
+
+test("after last query finishes", function() {
+  con.emit('readyForQuery');
+  test("emits drain", function() {
+    assert.ok(raisedDrain);
+  });
+});
+
