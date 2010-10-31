6eea7b3c4f01f47efbdce6cd5e069102ed1cd342
brianc <brian.m.carlson@gmail.com>
2010-10-31 14:49:49 -0500

    added unit test for connection propagating stream errors

---
+++ test/unit/connection/error-tests.js
@@ -0,0 +1,10 @@
+var helper = require(__dirname + '/test-helper');
+
+var con = new Connection({stream: new MemoryStream()});
+test("connection emits stream errors", function() {
+  assert.emits(con, 'error', function(err) {
+    assert.equal(err, "OMG!");
+  });
+  con.connect();
+  con.stream.emit('error', "OMG!");
+});
