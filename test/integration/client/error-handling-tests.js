9d090bc1bf170cbbf2dc15de3354351ed4f035c7
Paul Collier <paul@paulcollier.ca>
2010-11-01 18:06:44 -0700

    Propagate errors during queries to the Query

--- test/integration/client/error-handling-tests.js
+++ test/integration/client/error-handling-tests.js
@@ -2,8 +2,8 @@ var helper = require(__dirname + '/test-helper');
 
 test('error handling', function(){
   var client = helper.client();
-  client.query("select omfg from yodas_soda where pixistix = 'zoiks!!!'");
-  assert.emits(client, 'error', function(error) {
+  var query = client.query("select omfg from yodas_soda where pixistix = 'zoiks!!!'");
+  assert.emits(query, 'error', function(error) {
     assert.equal(error.severity, "ERROR");
     client.end();
   });
