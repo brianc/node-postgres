91aac6d3507e3102e1695490768914f11dea3e29
Brian Carlson <brian.m.carlson@gmail.com>
2010-11-15 00:10:21 -0600

    initial callback api integration underway

--- test/integration/client/api-tests.js
+++ test/integration/client/api-tests.js
@@ -1,9 +1,39 @@
 var helper = require(__dirname + '/../test-helper');
 var pg = require(__dirname + '/../../../lib');
 
-var connected = false;
-pg.connect(helper.args, function(err) {
-  connected = true;
-});
+var connected = false
+var called = false
+pg.connect(helper.args, function(err, client) {
+  connected = true
+  test('error is null', function() {
+    assert.equal(err, null)
+  })
 
-assert.ok(connected);
+  test('query execution', function() {
+    client.query('CREATE TEMP TABLE band(name varchar(100))')
+    client.query("INSERT INTO band (name) VALUES ('dead black hearts')")
+    client.query("SELECT * FROM band WHERE name = 'dead black hearts'", function(err, result) {
+      called = true;
+      assert.equal(result.rows.pop().name, 'dead black hearts')
+    })
+  })
+
+})
+
+process.on('exit', function() {
+  assert.ok(connected, 'never connected')
+  assert.ok(called, 'query result callback was never called')
+})
+
+
+
+
+
+test('raises error if cannot connect', function() {
+  pg.connect({database:'asdlfkajsdf there is no way this is a real database, right?!'}, function(err, client) {
+    assert.ok(err, 'error was null')
+  })
+})
+
+
+
