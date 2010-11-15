0bde640bab2a2ca81ca95bd77a619d40db351770
Brian Carlson <brian.m.carlson@gmail.com>
2010-11-15 00:42:38 -0600

    overloaded "simple" query api to provide callback enabled prepared statement

--- test/integration/client/api-tests.js
+++ test/integration/client/api-tests.js
@@ -2,32 +2,37 @@ var helper = require(__dirname + '/../test-helper');
 var pg = require(__dirname + '/../../../lib');
 
 var connected = false
-var called = false
+var simpleCalled = false
+var preparedCalled = false
+
 pg.connect(helper.args, function(err, client) {
   connected = true
   test('error is null', function() {
     assert.equal(err, null)
   })
 
-  test('query execution', function() {
+  test('simple query execution', function() {
     client.query('CREATE TEMP TABLE band(name varchar(100))')
     client.query("INSERT INTO band (name) VALUES ('dead black hearts')")
     client.query("SELECT * FROM band WHERE name = 'dead black hearts'", function(err, result) {
-      called = true;
+      simpleCalled = true
       assert.equal(result.rows.pop().name, 'dead black hearts')
     })
   })
 
+  test('prepared statement execution', function() {
+    client.query('SELECT * FROM band WHERE name = $1', ['dead black hearts'], function(err, result) {
+      preparedCalled = true
+      assert.equal(result.rows.pop().name, 'dead black hearts')
+    })
+  })
 })
 
 process.on('exit', function() {
   assert.ok(connected, 'never connected')
-  assert.ok(called, 'query result callback was never called')
+  assert.ok(simpleCalled, 'query result callback was never called')
+  assert.ok(preparedCalled, 'prepared callback was never called')
 })
-
-
-
-
 
 test('raises error if cannot connect', function() {
   pg.connect({database:'asdlfkajsdf there is no way this is a real database, right?!'}, function(err, client) {
