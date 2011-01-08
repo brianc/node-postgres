a811fcd06929d7da3d1102d60f5c28182ba10384
Darwin <darwin.git@marianna.se>
2011-01-08 16:34:47 +0100

    test showing bug...

--- test/unit/client/query-tests.js
+++ test/unit/client/query-tests.js
@@ -5,3 +5,7 @@ test("testing dateParser", function() {
 	assert.equal(q.dateParser("2010-12-11 09:09:04").toUTCString(),new Date("2010-12-11 09:09:04 GMT").toUTCString());
 });
 
+test("testing 2dateParser", function() {
+	assert.equal(JSON.stringify(q.dateParser("2010-12-11 09:09:04.19")),"\"2010-12-11T09:09:04.190Z\"");
+});
+
