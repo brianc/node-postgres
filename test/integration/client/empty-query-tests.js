d4039588695d1285d0c889642d82541ee6ffb3fc
brianc <brian.m.carlson@gmail.com>
2011-03-03 18:23:02 +0000

    fix test failure

--- test/integration/client/empty-query-tests.js
+++ test/integration/client/empty-query-tests.js
@@ -2,8 +2,8 @@ var helper = require(__dirname+'/test-helper');
 var client = helper.client();
 
 test("empty query message handling", function() {
-  client.query("");
   assert.emits(client, 'drain', function() {
-    client.end.bind(client);
-  })
+    client.end();
+  });
+  client.query("");
 });
