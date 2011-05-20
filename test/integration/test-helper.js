a69928ee46805d2d47df7bea92e75e2da4c28e01
brianc <brian.m.carlson@gmail.com>
2011-05-19 19:18:43 -0500

    make tests for native always test native

--- test/integration/test-helper.js
+++ test/integration/test-helper.js
@@ -1,7 +1,9 @@
 var helper = require(__dirname + '/../test-helper');
 
+//TODO would this be better served set at ../test-helper?
 if(helper.args.native) {
   Client = require(__dirname + '/../../lib/native').Client;
+  helper.pg = helper.pg.native;
 }
 //export parent helper stuffs
 module.exports = helper;
