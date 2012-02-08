e0f6baac5d7b7b2743c859abb582be881a1daa0b
Nicolas Raynaud <github@nraynaud.com>
2012-02-08 23:05:16 +0100

    bind Buffer variables as binary values
    Avoids having to encode blobs in query.

--- test/unit/connection/outbound-sending-tests.js
+++ test/unit/connection/outbound-sending-tests.js
@@ -112,6 +112,33 @@ test('bind messages', function() {
       .join(true, 'B');
     assert.received(stream, expectedBuffer);
   });
+  
+  test('with named statement, portal, and buffer value', function() {
+    con.bind({
+      portal: 'bang',
+      statement: 'woo',
+      values: [1, 'hi', null, new Buffer('zing', 'UTF-8')]
+    });
+    var expectedBuffer = new BufferList()
+      .addCString('bang')  //portal name
+      .addCString('woo') //statement name
+      .addInt16(4)//value count
+      .addInt16(0)//string
+      .addInt16(0)//string
+      .addInt16(0)//string
+      .addInt16(1)//binary
+      .addInt16(4)
+      .addInt32(1)
+      .add(Buffer("1"))
+      .addInt32(2)
+      .add(Buffer("hi"))
+      .addInt32(-1)
+      .addInt32(4)
+      .add(new Buffer('zing', 'UTF-8'))
+      .addInt16(0)
+      .join(true, 'B');
+    assert.received(stream, expectedBuffer);
+  });
 });
 
 
