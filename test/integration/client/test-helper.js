66ea0249d8e29a6d6da1c5afc21483525ed7d2a2
brianc <brian.m.carlson@gmail.com>
2012-05-30 22:40:49 -0500

    add failing test for #130

--- test/integration/client/test-helper.js
+++ test/integration/client/test-helper.js
@@ -1,10 +1,3 @@
 var helper = require(__dirname+'/../test-helper');
 
-//creates a client from cli parameters
-helper.client = function() {
-  var client = new Client(helper.config);
-  client.connect();
-  return client;
-};
-
 module.exports = helper;
--- test/integration/test-helper.js
+++ test/integration/test-helper.js
@@ -1,10 +1,18 @@
 var helper = require(__dirname + '/../test-helper');
 
-//TODO would this be better served set at ../test-helper?
 if(helper.args.native) {
   Client = require(__dirname + '/../../lib/native');
+  helper.Client = Client;
   helper.pg = helper.pg.native;
 }
+
+//creates a client from cli parameters
+helper.client = function() {
+  var client = new Client(helper.config);
+  client.connect();
+  return client;
+};
+
 //export parent helper stuffs
 module.exports = helper;
 
