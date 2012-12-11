102a069bd2360783eb106b4bbcfa93b3f7fd7030
brianc <brian.m.carlson@gmail.com>
2012-12-10 23:25:26 -0600

    have native bindings emit proper result object on 'end' event - closes #219

--- test/integration/client/result-metadata-tests.js
+++ test/integration/client/result-metadata-tests.js
@@ -4,20 +4,29 @@ var pg = helper.pg;
 test('should return insert metadata', function() {
   pg.connect(helper.config, assert.calls(function(err, client) {
     assert.isNull(err);
+
     client.query("CREATE TEMP TABLE zugzug(name varchar(10))", assert.calls(function(err, result) {
       assert.isNull(err);
       assert.equal(result.oid, null);
       assert.equal(result.command, 'CREATE');
-      client.query("INSERT INTO zugzug(name) VALUES('more work?')", assert.calls(function(err, result) {
+
+      var q = client.query("INSERT INTO zugzug(name) VALUES('more work?')", assert.calls(function(err, result) {
         assert.equal(result.command, "INSERT");
         assert.equal(result.rowCount, 1);
+
         client.query('SELECT * FROM zugzug', assert.calls(function(err, result) {
           assert.isNull(err);
           assert.equal(result.rowCount, 1);
           assert.equal(result.command, 'SELECT');
           process.nextTick(pg.end.bind(pg));
-        }))
-      }))
-    }))
-  }))
-})
+        }));
+      }));
+
+      assert.emits(q, 'end', function(result) {
+        assert.equal(result.command, "INSERT");
+        assert.equal(result.rowCount, 1);
+      });
+
+    }));
+  }));
+});
