aadb2917cca86abdc092512aee76a8569468f989
brianc <brian.m.carlson@gmail.com>
2013-03-07 16:09:40 -0600

    fix broken test

--- test/integration/connection-pool/error-tests.js
+++ test/integration/connection-pool/error-tests.js
@@ -7,7 +7,6 @@ pg.defaults.poolSize = 2;
 
 var killIdleQuery = 'SELECT procpid, (SELECT pg_terminate_backend(procpid)) AS killed FROM pg_stat_activity WHERE current_query LIKE \'<IDLE>\'';
 
-return console.log('TEMP IGNORE THIS TO GET REST OF SUITE PASSING')
 //get first client
 pg.connect(helper.config, assert.success(function(client, done) {
   client.id = 1;
@@ -19,17 +18,12 @@ pg.connect(helper.config, assert.success(function(client, done) {
       assert.ok(error);
       assert.ok(brokenClient);
       assert.equal(client.id, brokenClient.id);
-      console.log('got pg error')
-      console.log('calling pg.end()')
-      //done2();
-      pg.end();
     });
     //kill the connection from client
     client2.query(killIdleQuery, assert.success(function(res) {
       //check to make sure client connection actually was killed
-      console.log('\nkilled query');
       assert.lengthIs(res.rows, 1);
-      done();
+      pg.end();
     }));
   }));
 }));
