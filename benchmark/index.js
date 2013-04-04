a28f1563476f18c0000e5efaff855deb54fedf3b
bmc <brian.m.carlson@gmail.com>
2013-04-04 11:40:37 -0500

    work in benchmark

--- benchmark/index.js
+++ benchmark/index.js
@@ -1,45 +1,42 @@
-var profiler = require('profiler');
-var Client = require(__dirname + '/../lib/client');
-var buffers = require(__dirname + '/../test/test-buffers');
-require(__dirname + '/../test/unit/test-helper');
-console.log('');
+var async = require('async');
+var max = 40000;
+var maxTimes = 3;
+var doLoops = function(bench, loops, times, cb) {
+  var start = new Date();
+  var count = 0;
 
-var stream = new MemoryStream();
-stream.readyState = 'open';
-var client = new Client({
-  stream: stream
-});
-
-var rowDescription = new buffers.rowDescription([{
-  name: 'name',
-  tableID: 1,
-  attributeNumber: 1,
-  dataTypeID: 25, //text
-  typeModifer: 0,
-  formatCode: 0 //text format
-}]);
-var row1 = buffers.dataRow(['Brian']);
-var row2 = buffers.dataRow(['Bob']);
-var row3 = buffers.dataRow(['The amazing power of the everlasting gobstopper']);
-var complete = buffers.commandComplete('SELECT 3');
-var ready = buffers.readyForQuery();
-var buffer = Buffer.concat([rowDescription, row1, row2, row3, complete, ready]);
-
-client.connect(assert.calls(function() {
-  client.connection.emit('readyForQuery');
-
-  var callCount = 0;
-  var max = 1000;
-  profiler.resume();
-  for(var i = 0; i < max; i++) {
-    //BEGIN BENCH
-    client.query('SELECT * FROM whatever WHERE this = "doesnt even matter"', function(err, res) {
-      callCount++;
-    });
-    //END BENCH
-    stream.emit('data', buffer);
+  var done = function() {
+    var duration = (new Date() - start)
+    var seconds = (duration / 1000);
+    console.log("%d ops/sec - (%d/%d)", ~~(loops/seconds), loops, seconds);
+    var next = loops * 10;
+    if(next > max) {
+      if(times > maxTimes) return cb();
+      times++;
+      next = max;
+    }
+    setTimeout(function() {
+      doLoops(bench, next, times, cb);
+    }, 100);
   }
-  profiler.pause();
-  assert.equal(callCount, max);
-}));
-client.connection.emit('readyForQuery');
+
+  var run = function() {
+    if(count++ >= loops){
+      return done();
+    }
+    bench(function() {
+      setImmediate(run);
+    });
+  }
+  run();
+}
+var bench = require(__dirname + '/simple-query-parsing');
+console.log();
+var benches = ['simple-query-parsing', 'prepared-statement-parsing'];
+async.forEachSeries(benches, function(name, cb) {
+  var bench = require(__dirname + '/' + name)();
+  console.log('starting ', name);
+  doLoops(bench, 100, 1, cb);
+}, function(err, res) {
+  console.log('done')
+})
