20017b084956704be2495aff7c2269d722c52de4
rpedela <rpedela@datalanche.com>
2013-07-11 16:35:23 -0600

    Add native and JS tests for escapeLiteral and escapeIdentifier.

---
+++ test/unit/connection/escape-tests.js
@@ -0,0 +1,113 @@
+require(__dirname + "/test-helper");
+
+test('escapeLiteral: no special characters', function() {
+  var client = createClient();
+  var expected = "'hello world'";
+  var actual = client.escapeLiteral('hello world');
+  assert.equal(expected, actual);
+});
+
+test('escapeLiteral: contains double quotes only', function() {
+  var client = createClient();
+  var expected = "'hello \" world'";
+  var actual = client.escapeLiteral('hello " world');
+  assert.equal(expected, actual);
+});
+
+test('escapeLiteral: contains single quotes only', function() {
+  var client = createClient();
+  var expected = "'hello \'\' world'";
+  var actual = client.escapeLiteral('hello \' world');
+  assert.equal(expected, actual);
+});
+
+test('escapeLiteral: contains backslashes only', function() {
+  var client = createClient();
+  var expected = " E'hello \\\\ world'";
+  var actual = client.escapeLiteral('hello \\ world');
+  assert.equal(expected, actual);
+});
+
+test('escapeLiteral: contains single quotes and double quotes', function() {
+  var client = createClient();
+  var expected = "'hello '' \" world'";
+  var actual = client.escapeLiteral('hello \' " world');
+  assert.equal(expected, actual);
+});
+
+test('escapeLiteral: contains double quotes and backslashes', function() {
+  var client = createClient();
+  var expected = " E'hello \\\\ \" world'";
+  var actual = client.escapeLiteral('hello \\ " world');
+  assert.equal(expected, actual);
+});
+
+test('escapeLiteral: contains single quotes and backslashes', function() {
+  var client = createClient();
+  var expected = " E'hello \\\\ '' world'";
+  var actual = client.escapeLiteral('hello \\ \' world');
+  assert.equal(expected, actual);
+});
+
+test('escapeLiteral: contains single quotes, double quotes, and backslashes', function() {
+  var client = createClient();
+  var expected = " E'hello \\\\ '' \" world'";
+  var actual = client.escapeLiteral('hello \\ \' " world');
+  assert.equal(expected, actual);
+});
+
+test('escapeIdentifier: no special characters', function() {
+  var client = createClient();
+  var expected = '"hello world"';
+  var actual = client.escapeIdentifier('hello world');
+  assert.equal(expected, actual);
+});
+
+test('escapeIdentifier: contains double quotes only', function() {
+  var client = createClient();
+  var expected = '"hello "" world"';
+  var actual = client.escapeIdentifier('hello " world');
+  assert.equal(expected, actual);
+});
+
+test('escapeIdentifier: contains single quotes only', function() {
+  var client = createClient();
+  var expected = '"hello \' world"';
+  var actual = client.escapeIdentifier('hello \' world');
+  assert.equal(expected, actual);
+});
+
+test('escapeIdentifier: contains backslashes only', function() {
+  var client = createClient();
+  var expected = '"hello \\ world"';
+  var actual = client.escapeIdentifier('hello \\ world');
+  assert.equal(expected, actual);
+});
+
+test('escapeIdentifier: contains single quotes and double quotes', function() {
+  var client = createClient();
+  var expected = '"hello \' "" world"';
+  var actual = client.escapeIdentifier('hello \' " world');
+  assert.equal(expected, actual);
+});
+
+test('escapeIdentifier: contains double quotes and backslashes', function() {
+  var client = createClient();
+  var expected = '"hello \\ "" world"';
+  var actual = client.escapeIdentifier('hello \\ " world');
+  assert.equal(expected, actual);
+});
+
+test('escapeIdentifier: contains single quotes and backslashes', function() {
+  var client = createClient();
+  var expected = '"hello \\ \' world"';
+  var actual = client.escapeIdentifier('hello \\ \' world');
+  assert.equal(expected, actual);
+});
+
+test('escapeIdentifier: contains single quotes, double quotes, and backslashes', function() {
+  var client = createClient();
+  var expected = '"hello \\ \' "" world"';
+  var actual = client.escapeIdentifier('hello \\ \' " world');
+  assert.equal(expected, actual);
+});
