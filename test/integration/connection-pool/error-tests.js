343caefb75631bc5f062cee16c04eccd310f5218
Brian Carlson <brian.m.carlson@gmail.com>
2013-07-23 09:51:48 -0500

    Fix race in error handling test

--- test/integration/connection-pool/error-tests.js
+++ test/integration/connection-pool/error-tests.js
@@ -13,23 +13,27 @@ pg.connect(helper.config, assert.success(function(client, done) {
       client2.id = 2;
       var pidColName = 'procpid'
       helper.versionGTE(client2, '9.2.0', assert.success(function(isGreater) {
+        console.log(isGreater)
         var killIdleQuery = 'SELECT pid, (SELECT pg_terminate_backend(pid)) AS killed FROM pg_stat_activity WHERE state = $1';
         var params = ['idle'];
         if(!isGreater) {
           killIdleQuery = 'SELECT procpid, (SELECT pg_terminate_backend(procpid)) AS killed FROM pg_stat_activity WHERE current_query LIKE $1';
           params = ['%IDLE%']
         }
-        done2();
+
         //subscribe to the pg error event
         assert.emits(pg, 'error', function(error, brokenClient) {
           assert.ok(error);
           assert.ok(brokenClient);
           assert.equal(client.id, brokenClient.id);
         });
+
         //kill the connection from client
         client2.query(killIdleQuery, params, assert.success(function(res) {
           //check to make sure client connection actually was killed
           assert.lengthIs(res.rows, 1);
+          //return client2 to the pool
+          done2();
           pg.end();
         }));
       }));
