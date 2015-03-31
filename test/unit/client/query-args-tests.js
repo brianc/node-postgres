0ceda5f8180f3549798345ea2225f1cee409712f
Nicola Gervasi <nicola@sintpro.com>
2015-03-31 13:54:38 +0200

    tests

--- test/unit/client/query-args-tests.js
+++ test/unit/client/query-args-tests.js
@@ -1,44 +1,5 @@
-var helper = require(__dirname + '/test-helper');
 /*
-test('check that passed values types have not been changed during the query phase', function() {
-	var client = helper.client();
-	client.connect(assert.success(function() {
-		var originalValues = [1,new Date()];
-		var values = originalValues.slice();
-		var config = {
-			text: 'SELECT 1 WHERE 0 <> $1 AND 0 <> $2',
-			values: values
-		};
-		client.query(config, function(result) {
-			assert.equal(result.rows.length, 1);
-			console.log('result:',result.rows[0]);
-			assert.equal(values.length,originalValues.length,'expecting same length as given array!');
-			assert.strictEqual(isNaN(values[0]),false,'expecting a number!');
-			assert.strictEqual(values[1] instanceof Date,true,'expecting a Date object!');
-			client.end();
-		});
-	}));
-});
-
-test('check that passed values types have not been changed during the query phase', function() {
-	var client = helper.client();
-
-	var originalValues = [1,new Date()];
-	var values = originalValues.slice();
-	var config = {
-		text: 'SELECT 1 WHERE 0 <> $1 AND 0 <> $2',
-		values: values
-	};
-	client.query(config, function(result) {
-		assert.equal(result.rows.length, 1);
-		console.log('result:',result.rows[0]);
-		assert.equal(values.length,originalValues.length,'expecting same length as given array!');
-		assert.strictEqual(isNaN(values[0]),false,'expecting a number!');
-		assert.strictEqual(values[1] instanceof Date,true,'expecting a Date object!');
-		client.end();
-	});
-});
-*/
+var helper = require(__dirname+'/test-helper');
 var client = helper.client();
 
 test('check that passed values types have not been changed during the query phase', assert.calls(function() {
@@ -53,4 +14,27 @@ test('check that passed values types have not been changed during the query phas
 		assert.strictEqual(isNaN(values[0]),false,'expecting a number!');
 		assert.strictEqual(values[1] instanceof Date,true,'expecting a Date object!');
 	})
-}));
+})); */
+
+var helper = require(__dirname + '/test-helper');
+var pg = helper.pg;
+var config = helper.config;
+
+test('check that passed values types have not been changed during the query phase', function() {
+
+	pg.connect(config, assert.success(function(client, done) {
+		var originalValues = [1,new Date()];
+		var values = originalValues.slice();
+
+		client.query('SELECT 1 WHERE 0 <> $1 AND 0 <> $2',values, assert.success(function(err,result) {
+			assert.isNull(err);
+			assert.equal(result.rows.length, 1);
+			console.log('result:',result.rows[0]);
+			assert.equal(values.length,originalValues.length,'expecting same length as given array!');
+			assert.strictEqual(isNaN(values[0]),false,'expecting a number!');
+			assert.strictEqual(values[1] instanceof Date,true,'expecting a Date object!');
+			done();
+		}));
+
+	}));
+});
