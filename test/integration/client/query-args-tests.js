d51db34cb9d1479dfd72ab230e8fb0a5f6780f9b
Nicola Gervasi <nicola@sintpro.com>
2015-03-31 15:36:53 +0200

    tests

--- test/integration/client/query-args-tests.js
+++ test/integration/client/query-args-tests.js
@@ -8,14 +8,14 @@ test('check that passed values types have not been changed during the query phas
 		var originalValues = [1,2];
 		var values = originalValues.slice();
 
-		client.query('SELECT 1 WHERE 0 <> $1 AND 0 <> $2',values, assert.success(function(result) {
+		client.query('SELECT 1 WHERE 0 <> $1 AND 0 <> $2',values, function(result) {
 			console.log('result:',result);
 			assert.equal(result.rowCount, 1);
 			assert.equal(values.length,originalValues.length,'expecting same length as given array!');
 			assert.strictEqual(isNaN(values[0]),false,'expecting a number!');
 			assert.strictEqual(isNaN(values[1]),false,'expecting a number!');
 			done();
-		}));
+		});
 
 	}));
 });
