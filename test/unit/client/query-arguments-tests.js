65a8b3e0bda7b316100262e13155996059b1ef5b
Nicola Gervasi <nicola@sintpro.com>
2015-03-31 03:39:19 +0200

    again...

--- test/unit/client/query-arguments-tests.js
+++ test/unit/client/query-arguments-tests.js
@@ -4,19 +4,24 @@ test('check that passed arguments types are not changed during the query phase',
 	var client = helper.client();
 	var originalArguments = [1,new Date()];
 	var arguments = originalArguments.slice();
-	//var query = client.query('SELECT 1 WHERE 0 <> $1 AND 0 <> $2',arguments);
-
-
+	
 	var query = client.query({
 		text: 'SELECT 1 WHERE 0 <> $1 AND 0 <> $2',
 		values: arguments
 	});
 
+	assert.emits(query,'end', function() {
+		test('parse argument', function() {
+			assert.equal(arguments.length,originalArguments.length,'expecting same length as given array!');
+			assert.strictEqual(isNaN(arguments[0]),false,'expecting a number!');
+			assert.strictEqual(arguments[1] instanceof Date,true,'expecting a Date object!');
+		});
+	});
+
 	// Check for length
-	assert.equal(arguments.length,originalArguments.length,'expecting same length as given array!');
+
 	// Check values types
-	assert.strictEqual(isNaN(arguments[0]),false,'expecting a number!');
-	assert.strictEqual(arguments[1] instanceof Date,true,'expecting a Date object!');
+
 	/*
 	client.query('SELECT 1 WHERE 0 <> $1 AND 0 <> $2',arguments, function(err, result) {
 		assert.equal(err,null,'expecting no error');
