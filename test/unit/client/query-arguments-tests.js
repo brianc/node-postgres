1a2575bc89a25cd1a0504c73164a462aa196b031
Nicola Gervasi <nicola@sintpro.com>
2015-03-31 03:59:13 +0200

    tests

--- test/unit/client/query-arguments-tests.js
+++ test/unit/client/query-arguments-tests.js
@@ -1,17 +1,16 @@
 var helper = require(__dirname + "/test-helper");
 
-var client = helper.client();
-var originalArguments = [1,new Date()];
-var arguments = originalArguments.slice();
+test('check that passed arguments types have not been changed during the query phase', function() {
+	var client = helper.client();
+	var originalArguments = [1,new Date()];
+	var arguments = originalArguments.slice();
 
-client.query('SELECT 1 WHERE 0 <> $1 AND 0 <> $2',arguments);
-client.on('row', function(row) {
-	test('check that passed arguments types have not been changed during the query phase', function() {
-		assert.notEqual(row,undefined);
-		assert.notEqual(row,null);
+	client.query('SELECT 1 WHERE 0 <> $1 AND 0 <> $2',arguments, function(err,result) {
+		assert.isNull(err);
+		assert.notEqual(result,undefined);
+		assert.equal(result.rows.length,1,'expecting 1 row');
 		assert.equal(arguments.length,originalArguments.length,'expecting same length as given array!');
 		assert.strictEqual(isNaN(arguments[0]),false,'expecting a number!');
 		assert.strictEqual(arguments[1] instanceof Date,true,'expecting a Date object!');
 	});
 });
-
