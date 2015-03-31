e703826c4615d63644dd0c1e21c9892d6b502d69
Nicola Gervasi <nicola@sintpro.com>
2015-03-31 13:38:58 +0200

    tests

--- test/unit/client/query-args-tests.js
+++ test/unit/client/query-args-tests.js
@@ -1,5 +1,5 @@
 var helper = require(__dirname + '/test-helper');
-
+/*
 test('check that passed values types have not been changed during the query phase', function() {
 	var client = helper.client();
 	client.connect(assert.success(function() {
@@ -18,4 +18,23 @@ test('check that passed values types have not been changed during the query phas
 			client.end();
 		});
 	}));
+});
+*/
+test('check that passed values types have not been changed during the query phase', function() {
+	var client = helper.client();
+
+	var originalValues = [1,new Date()];
+	var values = originalValues.slice();
+	var config = {
+		text: 'SELECT 1 WHERE 0 <> $1 AND 0 <> $2',
+		values: values
+	};
+	client.query(config, function(result) {
+		assert.equal(result.rows.length, 1);
+		console.log('result:',result.rows[0]);
+		assert.equal(values.length,originalValues.length,'expecting same length as given array!');
+		assert.strictEqual(isNaN(values[0]),false,'expecting a number!');
+		assert.strictEqual(values[1] instanceof Date,true,'expecting a Date object!');
+		client.end();
+	});
 });
