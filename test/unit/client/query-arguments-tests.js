6f4921b5a1dcfe9b31f91c94b0653b92350637f6
Nicola Gervasi <nicola@sintpro.com>
2015-03-31 02:42:51 +0200

    Tests for issue 750

---
+++ test/unit/client/query-arguments-tests.js
@@ -0,0 +1,15 @@
+var helper = require(__dirname + "/test-helper");
+
+test('check that passed arguments types are not changed during the query phase', function() {
+	var client = helper.client();
+	var originalArguments = [1,2,3];
+	var arguments = originalArguments.slice();
+	client.query('SELECT 1 WHERE 0 <> $1 AND 0 <> $2 AND 0 <> $3',arguments, function() {
+		// Check for length
+		assert.equal(arguments.length,originalArguments.length);
+		// Check values
+		assert.equal(arguments[0],originalArguments[0]);
+		assert.equal(arguments[1],originalArguments[1]);
+		assert.equal(arguments[2],originalArguments[2]);
+	});
+});
