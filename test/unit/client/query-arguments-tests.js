c86214db325e31997c5317eb242a89d4383ab111
Nicola Gervasi <nicola@sintpro.com>
2015-03-31 03:02:19 +0200

    test update

--- test/unit/client/query-arguments-tests.js
+++ test/unit/client/query-arguments-tests.js
@@ -7,9 +7,9 @@ test('check that passed arguments types are not changed during the query phase',
 	client.query('SELECT 1 WHERE 0 <> $1 AND 0 <> $2 AND 0 <> $3',arguments, function() {
 		// Check for length
 		assert.equal(arguments.length,originalArguments.length);
-		// Check values
-		assert.deepEqual(arguments[0],originalArguments[0]);
-		assert.deepEqual(arguments[1],originalArguments[1]);
-		assert.deepEqual(arguments[2],originalArguments[2]);
+		// Check values types, expect numbers, not strings
+		assert.deepEqual(isNaN(arguments[0]),true);
+		assert.deepEqual(isNaN(arguments[1]),true);
+		assert.deepEqual(isNaN(arguments[2]),true);
 	});
 });
