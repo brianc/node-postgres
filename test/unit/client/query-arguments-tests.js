b58362f0adec3fa3fac45f9fea44af9633b1aede
Nicola Gervasi <nicola@sintpro.com>
2015-03-31 03:08:19 +0200

    test update

--- test/unit/client/query-arguments-tests.js
+++ test/unit/client/query-arguments-tests.js
@@ -2,14 +2,13 @@ var helper = require(__dirname + "/test-helper");
 
 test('check that passed arguments types are not changed during the query phase', function() {
 	var client = helper.client();
-	var originalArguments = [1,2,3];
+	var originalArguments = [1,new Date()];
 	var arguments = originalArguments.slice();
-	client.query('SELECT 1 WHERE 0 <> $1 AND 0 <> $2 AND 0 <> $3',arguments, function() {
+	client.query('SELECT 1 WHERE 0 <> $1 AND 0 <> $2',arguments, function() {
 		// Check for length
 		assert.equal(arguments.length,originalArguments.length);
-		// Check values types, expect numbers, not strings
-		assert.deepEqual(isNaN(arguments[0]),true);
-		assert.deepEqual(isNaN(arguments[1]),true);
-		assert.deepEqual(isNaN(arguments[2]),true);
+		// Check values types
+		assert.deepEqual(isNaN(arguments[0]),false);
+		assert.deepEqual(arguments[1] instanceof Date,true);
 	});
 });
