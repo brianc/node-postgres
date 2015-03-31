0ad20a6d667a352897a0df690561c66577bdd1dd
Nicola Gervasi <nicola@sintpro.com>
2015-03-31 03:44:49 +0200

    test update

--- test/unit/client/query-arguments-tests.js
+++ test/unit/client/query-arguments-tests.js
@@ -1,39 +1,20 @@
 var helper = require(__dirname + "/test-helper");
 
-test('check that passed arguments types are not changed during the query phase', function() {
+test('executing query', function() {
 	var client = helper.client();
 	var originalArguments = [1,new Date()];
 	var arguments = originalArguments.slice();
-	
+
 	var query = client.query({
 		text: 'SELECT 1 WHERE 0 <> $1 AND 0 <> $2',
 		values: arguments
 	});
 
-	assert.emits(query,'end', function() {
-		test('parse argument', function() {
+	assert.emits(query,'row', function(row) {
+		test('check that passed arguments types have not been changed during the query phase', function() {
 			assert.equal(arguments.length,originalArguments.length,'expecting same length as given array!');
 			assert.strictEqual(isNaN(arguments[0]),false,'expecting a number!');
 			assert.strictEqual(arguments[1] instanceof Date,true,'expecting a Date object!');
 		});
 	});
-
-	// Check for length
-
-	// Check values types
-
-	/*
-	client.query('SELECT 1 WHERE 0 <> $1 AND 0 <> $2',arguments, function(err, result) {
-		assert.equal(err,null,'expecting no error');
-		console.log('err:',err);
-		console.log('result:',result.rows[0]);
-		console.log(arguments);
-		assert.equal(result.rows.length,1,'expecting 1 row');
-		// Check for length
-		assert.equal(arguments.length,originalArguments.length,'expecting same lenght as given array!');
-		// Check values types
-		assert.strictEqual(isNaN(arguments[0]),false,'expecting a number!');
-		assert.strictEqual(arguments[1] instanceof Date,true,'expecting a Date object!');
-	});
-	*/
 });
