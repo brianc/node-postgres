2799cca4db200954675f04537731ae723b512c71
Nicola Gervasi <nicola@sintpro.com>
2015-03-31 13:45:31 +0200

    tests

--- test/unit/client/query-args-tests.js
+++ test/unit/client/query-args-tests.js
@@ -19,7 +19,7 @@ test('check that passed values types have not been changed during the query phas
 		});
 	}));
 });
-*/
+
 test('check that passed values types have not been changed during the query phase', function() {
 	var client = helper.client();
 
@@ -38,3 +38,19 @@ test('check that passed values types have not been changed during the query phas
 		client.end();
 	});
 });
+*/
+var client = helper.client();
+
+test('check that passed values types have not been changed during the query phase', assert.calls(function() {
+	var originalValues = [1,new Date()];
+	var values = originalValues.slice();
+
+	client.query('SELECT 1 WHERE 0 <> $1 AND 0 <> $2',values, function(err, result) {
+		assert.isNull(err);
+		assert.equal(result.rows.length, 1);
+		console.log('result:',result.rows[0]);
+		assert.equal(values.length,originalValues.length,'expecting same length as given array!');
+		assert.strictEqual(isNaN(values[0]),false,'expecting a number!');
+		assert.strictEqual(values[1] instanceof Date,true,'expecting a Date object!');
+	})
+}));
