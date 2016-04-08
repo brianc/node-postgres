55b90f1f6ceb90d08ae6cd84083190b6fdbf03f3
brianc <brian.m.carlson@gmail.com>
2016-04-08 18:41:05 -0500

    Only run native tests during native testing

--- test/integration/gh-issues/981-tests.js
+++ test/integration/gh-issues/981-tests.js
@@ -1,3 +1,10 @@
+var helper = require(__dirname + '/../test-helper');
+
+//native bindings are only installed for native tests
+if(!helper.args.native) {
+  return;
+}
+
 var assert = require('assert')
 var pg = require('../../../lib')
 var native = require('../../../lib').native
