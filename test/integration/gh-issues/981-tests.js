ebd1ce053f3d857f818d5807efc7b8c06664b1f7
brianc <brian.m.carlson@gmail.com>
2016-04-08 18:22:22 -0500

    Requiring native bindings polutes 'global'
    There was some nasty global-ish variable reference updating happening when the native module 'initializes' after its require with `require('pg').native`
    
    This fixes the issue by making sure both `require('pg')` and `require('pg').native` each initialize their own context in isolation and no weird global-ish references are used & subsequently stomped on.

---
+++ test/integration/gh-issues/981-tests.js
@@ -0,0 +1,20 @@
+var assert = require('assert')
+var pg = require('../../../lib')
+var native = require('../../../lib').native
+
+var JsClient = require('../../../lib/client')
+var NativeClient = require('../../../lib/native')
+
+assert(pg.Client === JsClient);
+assert(native.Client === NativeClient);
+
+pg.connect(function(err, client, done) {
+  assert(client instanceof JsClient);
+  client.end();
+
+  native.connect(function(err, client, done) {
+    assert(client instanceof NativeClient);
+    client.end();
+  });
+});
+
