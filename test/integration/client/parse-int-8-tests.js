e535c047d6b0cc54cc97953e5523e4e29b044e23
Alexander Mochalin <horroshow@mail.ru>
2016-10-19 01:21:17 +0500

    test

--- test/integration/client/parse-int-8-tests.js
+++ test/integration/client/parse-int-8-tests.js
@@ -6,11 +6,14 @@ test('ability to turn on and off parser', function() {
   pg.connect(helper.config, assert.success(function(client, done) {
     pg.defaults.parseInt8 = true;
     client.query('CREATE TEMP TABLE asdf(id SERIAL PRIMARY KEY)');
-    client.query('SELECT COUNT(*) as "count" FROM asdf', assert.success(function(res) {
+    client.query('SELECT COUNT(*) as "count", \'{1,2,3}\'::bigint[] as array FROM asdf', assert.success(function(res) {
+      assert.strictEqual(0, res.rows[0].count);
+      assert.strictEqual([1, 2, 3], res.rows[0].array);
       pg.defaults.parseInt8 = false;
-      client.query('SELECT COUNT(*) as "count" FROM asdf', assert.success(function(res) {
+      client.query('SELECT COUNT(*) as "count", \'{1,2,3}\'::bigint[] as array FROM asdf', assert.success(function(res) {
         done();
         assert.strictEqual("0", res.rows[0].count);
+        assert.strictEqual(['1', '2', '3'], res.rows[0].array);
         pg.end();
       }));
     }));
