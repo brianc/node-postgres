f6c40b9331c90d794d5fcbb1d4ae2f28eabd4d42
Alexander Mochalin <horroshow@mail.ru>
2016-12-16 20:44:19 +0500

    parse int8[] (#1152)
    * parse int8[]
    
    * missing semicolon
    
    * test
    
    * test fixed
    
    * test fixed
    
    * test fixed. again.

--- test/integration/client/parse-int-8-tests.js
+++ test/integration/client/parse-int-8-tests.js
@@ -6,11 +6,18 @@ test('ability to turn on and off parser', function() {
   pg.connect(helper.config, assert.success(function(client, done) {
     pg.defaults.parseInt8 = true;
     client.query('CREATE TEMP TABLE asdf(id SERIAL PRIMARY KEY)');
-    client.query('SELECT COUNT(*) as "count" FROM asdf', assert.success(function(res) {
+    client.query('SELECT COUNT(*) as "count", \'{1,2,3}\'::bigint[] as array FROM asdf', assert.success(function(res) {
+      assert.strictEqual(0, res.rows[0].count);
+      assert.strictEqual(1, res.rows[0].array[0]);
+      assert.strictEqual(2, res.rows[0].array[1]);
+      assert.strictEqual(3, res.rows[0].array[2]);
       pg.defaults.parseInt8 = false;
-      client.query('SELECT COUNT(*) as "count" FROM asdf', assert.success(function(res) {
+      client.query('SELECT COUNT(*) as "count", \'{1,2,3}\'::bigint[] as array FROM asdf', assert.success(function(res) {
         done();
-        assert.strictEqual("0", res.rows[0].count);
+        assert.strictEqual('0', res.rows[0].count);
+        assert.strictEqual('1', res.rows[0].array[0]);
+        assert.strictEqual('2', res.rows[0].array[1]);
+        assert.strictEqual('3', res.rows[0].array[2]);
         pg.end();
       }));
     }));
