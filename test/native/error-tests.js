218d24957aaadf0390c391804d69c97097b3d7c4
Brian M. Carlson <brian.m.carlson@gmail.com>
2017-06-13 11:43:15 -0500

    Remove promise-interop from native query

--- test/native/error-tests.js
+++ test/native/error-tests.js
@@ -1,34 +1,6 @@
 var helper = require(__dirname + "/../test-helper");
 var Client = require(__dirname + "/../../lib/native");
 
-test('query with non-text as first parameter throws error', function() {
-  var client = new Client(helper.config);
-  client.connect();
-  assert.emits(client, 'connect', function() {
-    client.end();
-    assert.emits(client, 'end', function() {
-      assert.throws(function() {
-        client.query({text:{fail: true}});
-      });
-    });
-  });
-});
-
-test('parameterized query with non-text as first parameter throws error', function() {
-  var client = new Client(helper.config);
-  client.connect();
-  assert.emits(client, 'connect', function() {
-    client.end();
-    assert.emits(client, 'end', function() {
-      assert.throws(function() {
-        client.query({
-          text: {fail: true},
-          values: [1, 2]
-        })
-      });
-    });
-  });
-});
 
 var connect = function(callback) {
   var client = new Client(helper.config);
