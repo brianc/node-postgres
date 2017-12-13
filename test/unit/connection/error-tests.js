70258fc334769bb4f8b63733b946f6638629ac88
Pasi Eronen <pe@iki.fi>
2017-12-13 12:49:06 +0200

    Ignore socket hangup when ending connection also with ssl (#1344).

--- test/unit/connection/error-tests.js
+++ test/unit/connection/error-tests.js
@@ -1,31 +1,62 @@
 'use strict'
 var helper = require(__dirname + '/test-helper')
 var Connection = require(__dirname + '/../../../lib/connection')
-test('connection emits stream errors', function () {
+var net = require('net')
+
+const suite = new helper.Suite()
+
+suite.test('connection emits stream errors', function (done) {
   var con = new Connection({stream: new MemoryStream()})
   assert.emits(con, 'error', function (err) {
     assert.equal(err.message, 'OMG!')
+    done()
   })
   con.connect()
   con.stream.emit('error', new Error('OMG!'))
 })
 
-test('connection emits ECONNRESET errors during normal operation', function () {
+suite.test('connection emits ECONNRESET errors during normal operation', function (done) {
   var con = new Connection({stream: new MemoryStream()})
   con.connect()
   assert.emits(con, 'error', function (err) {
     assert.equal(err.code, 'ECONNRESET')
+    done()
   })
   var e = new Error('Connection Reset')
   e.code = 'ECONNRESET'
   con.stream.emit('error', e)
 })
 
-test('connection does not emit ECONNRESET errors during disconnect', function () {
+suite.test('connection does not emit ECONNRESET errors during disconnect', function (done) {
   var con = new Connection({stream: new MemoryStream()})
   con.connect()
   var e = new Error('Connection Reset')
   e.code = 'ECONNRESET'
   con.end()
   con.stream.emit('error', e)
+  done()
+})
+
+
+suite.test('connection does not emit ECONNRESET errors during disconnect also when using SSL', function (done) {
+  // our fake postgres server, which just responds with 'S' to start SSL
+  var socket
+  var server = net.createServer(function (c) {
+    socket = c
+    c.once('data', function (data) {
+      c.write(new Buffer('S', 'utf8'))
+    })
+  })
+
+  server.listen(7778, function () {
+    var con = new Connection({ssl: true})
+    con.connect(7778, 'localhost')
+    assert.emits(con, 'sslconnect', function () {
+      con.end()
+      socket.destroy()
+      server.close()
+      done()
+    })
+    con.requestSsl()
+  })
 })
