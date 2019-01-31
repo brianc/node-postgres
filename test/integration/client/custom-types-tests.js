e585975011bb49d6d48d1843be0699e0547b472d
Tony Wooster <tony.wooster@contentful.com>
2019-01-31 18:53:35 +0100

    Add support for per per-query types
    The documentation states that you can pass custom type processors to
    query objects. See:
    
    https://node-postgres.com/features/queries#types
    
    This didn't actually work. This commit adds an initial implementation
    of per-query type-parsing. Caveats:
    
    * It does not work with pg-native. That would require a separate pull
      request to pg-native, and a rather significant change to how that
      library handles query results.
    
    * Per-query types do not "inherit" from types assigned to the Client,
      ala TypeOverrides.

--- test/integration/client/custom-types-tests.js
+++ test/integration/client/custom-types-tests.js
@@ -3,13 +3,13 @@ const helper = require('./test-helper')
 const Client = helper.pg.Client
 const suite = new helper.Suite()
 
-const client = new Client({
-  types: {
-    getTypeParser: () => () => 'okay!'
-  }
-})
+const customTypes = {
+  getTypeParser: () => () => 'okay!'
+}
 
 suite.test('custom type parser in client config', (done) => {
+  const client = new Client({ types: customTypes })
+
   client.connect()
     .then(() => {
       client.query('SELECT NOW() as val', assert.success(function (res) {
@@ -18,3 +18,21 @@ suite.test('custom type parser in client config', (done) => {
       }))
     })
 })
+
+// Custom type-parsers per query are not supported in native
+if (!helper.args.native) {
+  suite.test('custom type parser in query', (done) => {
+    const client = new Client()
+
+    client.connect()
+      .then(() => {
+        client.query({
+          text: 'SELECT NOW() as val',
+          types: customTypes
+        }, assert.success(function (res) {
+          assert.equal(res.rows[0].val, 'okay!')
+          client.end().then(done)
+        }))
+      })
+  })
+}
