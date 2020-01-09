6425d8b75dd3701433f26f34e8f95ab142bd31f0
Brian M. Carlson <brian.m.carlson@gmail.com>
2020-01-09 11:03:12 -0600

    Result.fields should always be an array
    This fixes a subtle backwards incompatible change.  Added a test to prevent further regressions.  Closes #2056

---
+++ packages/pg/test/integration/gh-issues/2056-tests.js
@@ -0,0 +1,22 @@
+
+"use strict"
+var helper = require('./../test-helper')
+var assert = require('assert')
+
+const suite = new helper.Suite()
+
+suite.test('All queries should return a result array', (done) => {
+  const client = new helper.pg.Client()
+  client.connect()
+  const promises = []
+  promises.push(client.query('CREATE TEMP TABLE foo(bar TEXT)'))
+  promises.push(client.query('INSERT INTO foo(bar) VALUES($1)', ['qux']))
+  promises.push(client.query('SELECT * FROM foo WHERE bar = $1', ['foo']))
+  Promise.all(promises).then(results => {
+    results.forEach(res => {
+      assert(Array.isArray(res.fields))
+      assert(Array.isArray(res.rows))
+    })
+    client.end(done)
+  })
+})
