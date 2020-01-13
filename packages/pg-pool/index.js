fa1d44accbb5c8201232d67899ab8819340428d2
Charmander <~@charmander.me>
2020-01-13 08:30:21 -0800

    Avoid creating password property on pool options
    when it didn’t exist previously.

--- packages/pg-pool/index.js
+++ packages/pg-pool/index.js
@@ -60,15 +60,19 @@ class Pool extends EventEmitter {
   constructor (options, Client) {
     super()
     this.options = Object.assign({}, options)
-    const password = this.options.password
-    // "hiding" the password so it doesn't show up in stack traces
-    // or if the client is console.logged
-    Object.defineProperty(this.options, 'password', {
-      configurable: true,
-      enumerable: false,
-      value: password,
-      writable: true
-    })
+
+    if ('password' in this.options) {
+      const password = this.options.password
+      // "hiding" the password so it doesn't show up in stack traces
+      // or if the client is console.logged
+      Object.defineProperty(this.options, 'password', {
+        configurable: true,
+        enumerable: false,
+        value: password,
+        writable: true
+      })
+    }
+
     this.options.max = this.options.max || this.options.poolSize || 10
     this.log = this.options.log || function () { }
     this.Client = this.options.Client || Client || require('pg').Client
