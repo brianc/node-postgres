50a8beebcd4f45b6028ebf2e28c631c0f13ed531
Brian M. Carlson <brian.m.carlson@gmail.com>
2020-02-25 10:41:13 -0600

    Enable old commented out ssl test, add test for self-signed cert failuer

--- packages/pg/test/integration/client/ssl-tests.js
+++ packages/pg/test/integration/client/ssl-tests.js
@@ -1,15 +1,28 @@
 'use strict'
-var pg = require(__dirname + '/../../../lib')
-var config = require(__dirname + '/test-helper').config
-test('can connect with ssl', function () {
-  return false
-  config.ssl = {
-    rejectUnauthorized: false
+const { pg, Suite } = require('./test-helper')
+const assert = require('assert')
+
+const suite = new Suite()
+
+suite.testAsync('it can connect w/ ssl', async () => {
+  const ssl = {
+    rejectUnauthorized: false,
   }
-  pg.connect(config, assert.success(function (client) {
-    return false
-    client.query('SELECT NOW()', assert.success(function () {
-      pg.end()
-    }))
-  }))
+
+  const client = new pg.Client({ ssl })
+  await client.connect()
+  assert.strictEqual(client.connection.stream.encrypted, true)
+  await client.end()
+})
+
+suite.testAsync('it rejects on self-signed cert', async () => {
+  const ssl = true
+  const client = new pg.Client({ ssl })
+  try {
+    await client.connect()
+  } catch (e) {
+    assert(e.message.includes('self signed'))
+    return
+  }
+  throw new Error('connection should have failed with self-signed cert error')
 })
