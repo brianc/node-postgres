34317bcfd2c153e963a74597e108da23874a9235
Mike Slattery <mslattery@stratusdata.com>
2020-04-01 19:05:48 -0700

    account for optional callback argument

--- packages/pg-pool/index.js
+++ packages/pg-pool/index.js
@@ -365,7 +365,7 @@ class Pool extends EventEmitter {
       return cb ? cb(err) : this.Promise.reject(err)
     }
     this.ending = true
-    this.force = force
+    this.force = cb === true || force
     const promised = promisify(this.Promise, cb)
     this._endCallback = promised.callback
     this._pulseQueue()
