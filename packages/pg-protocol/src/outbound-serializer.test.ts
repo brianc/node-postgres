be8d7a16cae1b71212037e299bd558378b0a1d81
Brian M. Carlson <brian.m.carlson@gmail.com>
2020-11-02 12:33:21 -0600

    Add another test for custom mapping

--- packages/pg-protocol/src/outbound-serializer.test.ts
+++ packages/pg-protocol/src/outbound-serializer.test.ts
@@ -129,6 +129,31 @@ describe('serializer', () => {
     })
   })
 
+  it('with custom valueMapper', function () {
+    const actual = serialize.bind({
+      portal: 'bang',
+      statement: 'woo',
+      values: ['1', 'hi', null, 'zing'],
+      valueMapper: () => null,
+    })
+    var expectedBuffer = new BufferList()
+      .addCString('bang') // portal name
+      .addCString('woo') // statement name
+      .addInt16(4)
+      .addInt16(0)
+      .addInt16(0)
+      .addInt16(0)
+      .addInt16(0)
+      .addInt16(4)
+      .addInt32(-1)
+      .addInt32(-1)
+      .addInt32(-1)
+      .addInt32(-1)
+      .addInt16(0)
+      .join(true, 'B')
+    assert.deepEqual(actual, expectedBuffer)
+  })
+
   it('with named statement, portal, and buffer value', function () {
     const actual = serialize.bind({
       portal: 'bang',
