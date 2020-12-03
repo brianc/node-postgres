341e80e9e576303495c263c1cb355ffde75a85ea
Matthieu Sieben <matthieusieben@users.noreply.github.com>
2020-12-03 12:38:04 +0100

    reject parsing promise on stream error

--- packages/pg-protocol/src/index.ts
+++ packages/pg-protocol/src/index.ts
@@ -5,7 +5,10 @@ import { Parser, MessageCallback } from './parser'
 export function parse(stream: NodeJS.ReadableStream, callback: MessageCallback): Promise<void> {
   const parser = new Parser()
   stream.on('data', (buffer: Buffer) => parser.parse(buffer, callback))
-  return new Promise((resolve) => stream.on('end', () => resolve()))
+  return new Promise((resolve, reject) => {
+    stream.on('end', () => resolve())
+    stream.on('error', (err) => reject(err))
+  })
 }
 
 export { serialize, DatabaseError }
