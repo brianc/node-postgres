a1a923b0b638da3ae3c5c9334e4f734d0b9f44a4
Sehrope Sarkuni <sehrope@jackdb.com>
2020-12-29 09:13:33 -0500

    Tighten arg checks in SASL xorBuffers(...)

--- packages/pg/lib/sasl.js
+++ packages/pg/lib/sasl.js
@@ -113,19 +113,19 @@ function extractVariablesFromFirstServerMessage(data) {
 }
 
 function xorBuffers(a, b) {
-  if (!Buffer.isBuffer(a)) a = Buffer.from(a)
-  if (!Buffer.isBuffer(b)) b = Buffer.from(b)
-  var res = []
-  if (a.length > b.length) {
-    for (var i = 0; i < b.length; i++) {
-      res.push(a[i] ^ b[i])
-    }
-  } else {
-    for (var j = 0; j < a.length; j++) {
-      res.push(a[j] ^ b[j])
-    }
+  if (!Buffer.isBuffer(a)) {
+    throw new TypeError('first argument must be a Buffer')
   }
-  return Buffer.from(res)
+  if (!Buffer.isBuffer(b)) {
+    throw new TypeError('second argument must be a Buffer')
+  }
+  if (a.length !== b.length) {
+    throw new Error('Buffer lengths must match')
+  }
+  if (a.length === 0) {
+    throw new Error('Buffers cannot be empty')
+  }
+  return Buffer.from(a.map((_, i) => a[i] ^ b[i]))
 }
 
 function sha256(text) {
