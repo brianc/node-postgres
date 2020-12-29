736173f3cbbd63f8e9b5b73e7cc9e8bc51dc7c38
Sehrope Sarkuni <sehrope@jackdb.com>
2020-12-29 12:43:33 -0500

    Fix SCRAM tests
    Removes custom assert.throws(...) so that the real one from the assert package is used and
    fixes the SCRAM tests to reflect the updated error messages and actual checking of errors.
    
    Previously the custom assert.throws(...) was ignoring the error signature validation.

--- packages/pg/test/test-helper.js
+++ packages/pg/test/test-helper.js
@@ -111,16 +111,6 @@ assert.success = function (callback) {
   }
 }
 
-assert.throws = function (offender) {
-  try {
-    offender()
-  } catch (e) {
-    assert.ok(e instanceof Error, 'Expected ' + offender + ' to throw instances of Error')
-    return
-  }
-  assert.ok(false, 'Expected ' + offender + ' to throw exception')
-}
-
 assert.lengthIs = function (actual, expectedLength) {
   assert.equal(actual.length, expectedLength)
 }
