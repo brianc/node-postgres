d544e07a711d609f3fe1c5263f5ef7e0f6a44be0
Ryan Staples <42743016+Srayman@users.noreply.github.com>
2022-11-15 10:08:46 -0500

    change instanceof to isDate

--- packages/pg/lib/utils.js
+++ packages/pg/lib/utils.js
@@ -4,6 +4,8 @@ const crypto = require('crypto')
 
 const defaults = require('./defaults')
 
+const util = require('util')
+
 function escapeElement(elementRepresentation) {
   var escaped = elementRepresentation.replace(/\\/g, '\\\\').replace(/"/g, '\\"')
 
@@ -52,7 +54,7 @@ var prepareValue = function (val, seen) {
     }
     return buf.slice(val.byteOffset, val.byteOffset + val.byteLength) // Node.js v4 does not support those Buffer.from params
   }
-  if (val instanceof Date) {
+  if (util.types.isDate(val)) {
     if (defaults.parseInputDatesAsUTC) {
       return dateToStringUTC(val)
     } else {
