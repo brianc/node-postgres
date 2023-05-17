23d7f5fd7b49eb0ebf905f6274ad45ce69c4dd4f
Charmander <~@charmander.me>
2023-05-16 19:32:02 -0700

    test: Remove unused `assert.UTCDate`

--- packages/pg/test/test-helper.js
+++ packages/pg/test/test-helper.js
@@ -49,29 +49,6 @@ assert.emits = function (item, eventName, callback, message) {
   })
 }
 
-assert.UTCDate = function (actual, year, month, day, hours, min, sec, milisecond) {
-  var actualYear = actual.getUTCFullYear()
-  assert.equal(actualYear, year, 'expected year ' + year + ' but got ' + actualYear)
-
-  var actualMonth = actual.getUTCMonth()
-  assert.equal(actualMonth, month, 'expected month ' + month + ' but got ' + actualMonth)
-
-  var actualDate = actual.getUTCDate()
-  assert.equal(actualDate, day, 'expected day ' + day + ' but got ' + actualDate)
-
-  var actualHours = actual.getUTCHours()
-  assert.equal(actualHours, hours, 'expected hours ' + hours + ' but got ' + actualHours)
-
-  var actualMin = actual.getUTCMinutes()
-  assert.equal(actualMin, min, 'expected min ' + min + ' but got ' + actualMin)
-
-  var actualSec = actual.getUTCSeconds()
-  assert.equal(actualSec, sec, 'expected sec ' + sec + ' but got ' + actualSec)
-
-  var actualMili = actual.getUTCMilliseconds()
-  assert.equal(actualMili, milisecond, 'expected milisecond ' + milisecond + ' but got ' + actualMili)
-}
-
 assert.equalBuffers = function (actual, expected) {
   if (actual.length != expected.length) {
     spit(actual, expected)
