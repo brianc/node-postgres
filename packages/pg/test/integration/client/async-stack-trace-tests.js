1d29d64edf9b1e4a0a32f5757d1990df25ca6d5d
phiresky <phireskyde+gh@gmail.com>
2023-05-31 18:28:53 +0200

    fix stack traces of query() to include the async context (#1762) (#2983)
    * fix stack traces of query() to include the async context (#1762)
    
    * rename tests so they are actually run
    
    * conditionally only run async stack trace tests on node 16+
    
    * add stack trace to pg-native
    
    ---------
    
    Co-authored-by: Charmander <~@charmander.me>

---
+++ packages/pg/test/integration/client/async-stack-trace-tests.js
@@ -0,0 +1,51 @@
+'use strict'
+var helper = require('../test-helper')
+var pg = helper.pg
+
+process.on('unhandledRejection', function (e) {
+  console.error(e, e.stack)
+  process.exit(1)
+})
+
+const suite = new helper.Suite()
+
+// these tests will only work for if --async-stack-traces is on, which is the default starting in node 16.
+const NODE_MAJOR_VERSION = +process.versions.node.split('.')[0];
+if (NODE_MAJOR_VERSION >= 16) {
+  suite.testAsync('promise API async stack trace in pool', async function outerFunction() {
+    async function innerFunction() {
+      const pool = new pg.Pool()
+      await pool.query('SELECT test from nonexistent');
+    }
+    try {
+      await innerFunction();
+      throw Error("should have errored");
+    } catch (e) {
+      const stack = e.stack;
+      if(!e.stack.includes("innerFunction") || !e.stack.includes("outerFunction")) {
+        throw Error("async stack trace does not contain wanted values: " + stack);
+      }
+    }
+  })
+
+  suite.testAsync('promise API async stack trace in client', async function outerFunction() {
+    async function innerFunction() {
+      const client = new pg.Client()
+      await client.connect();
+      try {
+        await client.query('SELECT test from nonexistent');
+      } finally {
+        client.end();
+      }
+    }
+    try {
+      await innerFunction();
+      throw Error("should have errored");
+    } catch (e) {
+      const stack = e.stack;
+      if(!e.stack.includes("innerFunction") || !e.stack.includes("outerFunction")) {
+        throw Error("async stack trace does not contain wanted values: " + stack);
+      }
+    }
+  })
+}
