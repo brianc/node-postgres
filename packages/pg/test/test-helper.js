141c5526fa323d84b7555e43c5a452f7b2f5d0ed
alxndrsn <alxndrsn>
2024-06-06 07:46:54 +0000

    Reformat code to pass prettier rules

--- packages/pg/test/test-helper.js
+++ packages/pg/test/test-helper.js
@@ -72,11 +72,11 @@ assert.UTCDate = function (actual, year, month, day, hours, min, sec, milisecond
   assert.equal(actualMili, milisecond, 'expected milisecond ' + milisecond + ' but got ' + actualMili)
 }
 
-var spit = function(actual, expected) {
-  console.log("");
-  console.log("actual " + sys.inspect(actual));
-  console.log("expect " + sys.inspect(expected));
-  console.log("");
+const spit = function (actual, expected) {
+  console.log('')
+  console.log('actual ' + sys.inspect(actual))
+  console.log('expect ' + sys.inspect(expected))
+  console.log('')
 }
 
 assert.equalBuffers = function (actual, expected) {
