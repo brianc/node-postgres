4cdde759ca9fe09a362c7a03a213dce2e7bd559b
Ethan <6400075+ETHANTALJAFFE@users.noreply.github.com>
2024-08-18 13:28:42 +0300

    Updated node version check for crypto lib from 15 to 22

--- packages/pg/lib/crypto/utils.js
+++ packages/pg/lib/crypto/utils.js
@@ -1,6 +1,5 @@
 'use strict'
-
-const useLegacyCrypto = parseInt(process.versions && process.versions.node && process.versions.node.split('.')[0]) < 15
+const useLegacyCrypto = parseInt(process.versions && process.versions.node && process.versions.node.split('.')[0]) < 22
 if (useLegacyCrypto) {
   // We are on an old version of Node.js that requires legacy crypto utilities.
   module.exports = require('./utils-legacy')
