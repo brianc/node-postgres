1054ce79d4eef0a3512db9a637ec34ee708c7655
francesco <francesco.bagnoli.69@gmail.com>
2025-03-05 09:22:30 +0100

    fix: lint

--- packages/pg-pool/test/prioritizing-prepared-client.js
+++ packages/pg-pool/test/prioritizing-prepared-client.js
@@ -7,19 +7,20 @@ const it = require('mocha').it
 const Pool = require('../')
 
 describe('prioritizing prepared client', () => {
-  it('can create a single client with prepared statment and reuse it',
+  it(
+    'can create a single client with prepared statment and reuse it',
     co.wrap(function* () {
       const pool = new Pool({ max: 2 })
       expect(pool.waitingCount).to.equal(0)
 
       let res
-      
+
       res = yield pool.query({text: 'SELECT $1::text as name', values: ['hi'], name: 'foo'})
       expect(res.rows[0].name).to.equal('hi')
       expect(pool._idle.length).to.equal(1)
-      
-      res = yield pool.query({text: 'SELECT $1::text as name', values: ['hi'], name: 'foo'})
-      expect(res.rows[0].name).to.equal('hi')
+
+      res = yield pool.query({text: 'SELECT $1::text as name', values: ['ho'], name: 'foo'})
+      expect(res.rows[0].name).to.equal('ho')
       expect(pool._idle.length).to.equal(1)
 
       pool.end()
