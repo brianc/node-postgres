ae71ac7bc9dda102d5804b95a6ebfd88009e2aaa
francesco <francesco.bagnoli.69@gmail.com>
2025-03-05 09:19:23 +0100

    feat: add test

---
+++ packages/pg-pool/test/prioritizing-prepared-client.js
@@ -0,0 +1,29 @@
+const expect = require('expect.js')
+const co = require('co')
+
+const describe = require('mocha').describe
+const it = require('mocha').it
+
+const Pool = require('../')
+
+describe('prioritizing prepared client', () => {
+  it('can create a single client with prepared statment and reuse it',
+    co.wrap(function* () {
+      const pool = new Pool({ max: 2 })
+      expect(pool.waitingCount).to.equal(0)
+
+      let res
+      
+      res = yield pool.query({text: 'SELECT $1::text as name', values: ['hi'], name: 'foo'})
+      expect(res.rows[0].name).to.equal('hi')
+      expect(pool._idle.length).to.equal(1)
+      
+      res = yield pool.query({text: 'SELECT $1::text as name', values: ['hi'], name: 'foo'})
+      expect(res.rows[0].name).to.equal('hi')
+      expect(pool._idle.length).to.equal(1)
+
+      pool.end()
+    })
+  )
+
+})
