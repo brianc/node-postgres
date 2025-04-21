0d6c737c875026841f78044e9622ff7c08c4de5b
Brian Carlson <brian.m.carlson@gmail.com>
2025-04-21 17:06:58 -0500

    Use env vars for test connection in cf tests

--- packages/pg/test/cloudflare/vitest-cf.test.ts
+++ packages/pg/test/cloudflare/vitest-cf.test.ts
@@ -1,11 +1,10 @@
 import { Pool } from 'pg'
 import { test } from 'vitest'
+import args from '../../test/cli'
 import assert from 'node:assert'
 
 test('default', async () => {
-  const pool = new Pool({
-    connectionString: 'postgres://postgres:password@localhost:5432/postgres',
-  })
+  const pool = new Pool()
 
   const result = await pool.query('SELECT $1::text as name', ['cloudflare'])
   assert(result.rows[0].name === 'cloudflare')
