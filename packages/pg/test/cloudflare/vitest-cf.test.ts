cccda276e936222dd4cc12479b5b3499102285cf
Brian Carlson <brian.m.carlson@gmail.com>
2025-04-21 17:48:07 -0500

    Skip worker tests on node below 18

--- packages/pg/test/cloudflare/vitest-cf.test.ts
+++ packages/pg/test/cloudflare/vitest-cf.test.ts
@@ -5,7 +5,6 @@ import args from '../cli'
 
 test('default', async () => {
   const pool = new Pool(args)
-
   const result = await pool.query('SELECT $1::text as name', ['cloudflare'])
   assert(result.rows[0].name === 'cloudflare')
   pool.end()
