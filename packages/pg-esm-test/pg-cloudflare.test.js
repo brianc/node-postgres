6a9b873f5fabc2e0bf99680c41e40b987bfdc45e
Brian Carlson <brian.m.carlson@gmail.com>
2025-04-21 14:04:24 -0500

    Update PR copilot review

--- packages/pg-esm-test/pg-cloudflare.test.js
+++ packages/pg-esm-test/pg-cloudflare.test.js
@@ -2,7 +2,7 @@ import assert from 'node:assert'
 import { describe, it } from 'node:test'
 import { CloudflareSocket } from 'pg-cloudflare'
 
-describe('pg-pool', () => {
+describe('pg-cloudflare', () => {
   it('should export CloudflareSocket constructor', () => {
     assert.ok(new CloudflareSocket())
   })
