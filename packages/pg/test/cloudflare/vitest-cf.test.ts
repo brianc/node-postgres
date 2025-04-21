42d00fa9f694067913f22bc90560b145d64e4175
Brian Carlson <brian.m.carlson@gmail.com>
2025-04-21 17:24:03 -0500

    Fit vitest into existing legacy framework

--- packages/pg/test/cloudflare/vitest-cf.test.ts
+++ packages/pg/test/cloudflare/vitest-cf.test.ts
@@ -1,9 +1,10 @@
 import { Pool } from 'pg'
 import { test } from 'vitest'
 import assert from 'node:assert'
+import args from '../cli'
 
 test('default', async () => {
-  const pool = new Pool()
+  const pool = new Pool(args)
 
   const result = await pool.query('SELECT $1::text as name', ['cloudflare'])
   assert(result.rows[0].name === 'cloudflare')
--- packages/pg/test/vitest.config.mts
+++ packages/pg/test/vitest.config.mts
@@ -2,6 +2,7 @@ import { defineWorkersConfig } from '@cloudflare/vitest-pool-workers/config'
 
 export default defineWorkersConfig({
   test: {
+    watch: false,
     poolOptions: {
       workers: {
         wrangler: { configPath: './wrangler.jsonc' },
