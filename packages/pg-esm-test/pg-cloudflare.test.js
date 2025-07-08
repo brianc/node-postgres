0372fcff0600c9a318607fa0ded16f0366cb1df7
Brian Carlson <brian.m.carlson@gmail.com>
2025-07-08 18:11:40 -0500

    Add tests for export

--- packages/pg-esm-test/pg-cloudflare.test.js
+++ packages/pg-esm-test/pg-cloudflare.test.js
@@ -1,6 +1,7 @@
 import assert from 'node:assert'
 import { describe, it } from 'node:test'
 import { CloudflareSocket } from 'pg-cloudflare'
+import 'pg-cloudflare/package.json' assert { type: 'json' }
 
 describe('pg-cloudflare', () => {
   it('should export CloudflareSocket constructor', () => {
