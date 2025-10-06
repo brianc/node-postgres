4ce66f72e364236d6001526238bf2f42e5d678e9
Trenton Allan <allan.tr@northeastern.edu>
2025-10-06 07:01:06 -0400

    Fix: Remove indentation from blank lines in test

--- packages/pg/test/integration/gh-issues/2862-tests.js
+++ packages/pg/test/integration/gh-issues/2862-tests.js
@@ -9,7 +9,6 @@ const suite = new helper.Suite()
 suite.testAsync('Handle date objects as Date', async () => {
   const crossRealmDate = await vm.runInNewContext('new Date()')
   assert(!(crossRealmDate instanceof Date))
-  
   // Check if the cross-realm date is valid before using it
   const time = crossRealmDate.getTime()
   if (isNaN(time)) {
@@ -17,14 +16,11 @@ suite.testAsync('Handle date objects as Date', async () => {
     console.log('Skipping test - cross-realm date is invalid')
     return
   }
-  
   const date = new Date(time)
-  
   // Verify the date is valid before proceeding
   if (isNaN(date.getTime())) {
     throw new Error('Created invalid date from cross-realm date')
   }
-  
   const client = new helper.pg.Client()
   await client.connect()
   await client.query('CREATE TEMP TABLE foo(bar timestamptz, bar2 timestamptz)')
