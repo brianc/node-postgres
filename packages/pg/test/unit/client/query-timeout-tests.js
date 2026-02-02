e0d7cb4759730f1ec789119c41dcdb248cc505fc
uap-dev <devin.otway@gmail.com>
2026-02-02 13:50:12 -0800

    add querystream to test remove comments

--- packages/pg/test/unit/client/query-timeout-tests.js
+++ packages/pg/test/unit/client/query-timeout-tests.js
@@ -2,18 +2,10 @@
 
 const helper = require('./test-helper')
 const Query = require('../../../lib/query')
+const QueryStream = require('pg-query-stream')
 const assert = require('assert')
 const suite = new helper.Suite()
 const test = suite.test.bind(suite)
-
-// Regression tests for GitHub issue #1860
-// https://github.com/brianc/node-postgres/issues/1860
-//
-// When a Submittable (Query, QueryStream, Cursor) is passed to client.query()
-// with query_timeout configured but WITHOUT a callback, the client would crash
-// with "TypeError: queryCallback is not a function" when the timeout fires.
-//
-// The fix: queryCallback = query.callback || (() => {})
 
 test('query timeout with Submittable without callback delivers error via handleError', function (done) {
   const client = helper.client()
@@ -40,4 +32,18 @@ test('query timeout with Submittable with callback delivers error via callback',
     assert.equal(err.message, 'Query read timeout')
     done()
   })
+})
+
+test('query timeout with QueryStream without callback does not crash', function (done) {
+  const client = helper.client()
+  client.connectionParameters = { query_timeout: 10 }
+
+  const stream = new QueryStream('SELECT 1')
+  stream.handleError = (err) => {
+    assert.equal(err.message, 'Query read timeout')
+    done()
+  }
+
+  client.connection.emit('readyForQuery')
+  client.query(stream)
 })
