34499ce0583c9718ce80a0b8b995798d9b43711d
Simone Nigro <simone.nigro@acca.it>
2026-02-05 09:25:57 +0100

    fix: lint

--- packages/pg/lib/client.js
+++ packages/pg/lib/client.js
@@ -382,7 +382,8 @@ class Client extends EventEmitter {
       const currentQuery = this._pendingQueries[0]
       if (
         currentQuery &&
-        (currentQuery._gotRowDescription || currentQuery._gotError || currentQuery._gotCommandComplete)) {
+        (currentQuery._gotRowDescription || currentQuery._gotError || currentQuery._gotCommandComplete)
+      ) {
         this._pendingQueries.shift()
         currentQuery.handleReadyForQuery(this.connection)
       }
