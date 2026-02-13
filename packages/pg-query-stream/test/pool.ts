75d1d7dee8b3e538a7384cb6b2b63406a2ab648e
Brian Carlson <brian.m.carlson@gmail.com>
2026-02-13 14:46:00 -0600

    Remove log

--- packages/pg-query-stream/test/pool.ts
+++ packages/pg-query-stream/test/pool.ts
@@ -7,11 +7,10 @@ describe('pool', function () {
     const pool = new Pool()
     const query = new QueryStream('SELECT * FROM generate_series(0, 10) num', [])
     const q = pool.query(query)
-    console.log(q) // Promise { <pending>, ...
     query.on('data', (row) => {
       // just consume the whole stream
     })
-    await q //!
+    await q 
     query.on('end', () => {
       pool.end()
     })
