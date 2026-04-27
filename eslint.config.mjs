61928b6f1b809351bbd3240467bf27c088c1bde7
Chow Loong Jin <hyperair@debian.org>
2026-04-27 16:09:19 +0800

    eslint: Port config to new flat config format

---
+++ eslint.config.mjs
@@ -0,0 +1,66 @@
+import { defineConfig, globalIgnores } from "eslint/config";
+import typescriptEslint from "@typescript-eslint/eslint-plugin";
+import prettier from "eslint-plugin-prettier";
+import globals from "globals";
+import tsParser from "@typescript-eslint/parser";
+import path from "node:path";
+import { fileURLToPath } from "node:url";
+import js from "@eslint/js";
+import { FlatCompat } from "@eslint/eslintrc";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const compat = new FlatCompat({
+    baseDirectory: __dirname,
+    recommendedConfig: js.configs.recommended,
+    allConfig: js.configs.all
+});
+
+export default defineConfig([globalIgnores([
+    "**/node_modules",
+    "**/coverage",
+    "packages/*/dist",
+    "packages/pg-protocol/dist/**/*",
+    "packages/pg-query-stream/dist/**/*",
+]), {
+    extends: compat.extends("eslint:recommended", "plugin:prettier/recommended", "prettier"),
+
+    plugins: {
+        "@typescript-eslint": typescriptEslint,
+        prettier,
+    },
+
+    languageOptions: {
+        globals: {
+            ...globals.node,
+            ...globals.mocha,
+        },
+
+        parser: tsParser,
+        ecmaVersion: 2017,
+        sourceType: "module",
+    },
+
+    rules: {
+        "@typescript-eslint/no-unused-vars": ["error", {
+            args: "none",
+            caughtErrors: "none",
+            varsIgnorePattern: "^_$",
+        }],
+
+        // handled by @typescript-eslint/no-unused-vars
+        "no-unused-vars": "off",
+
+        "no-var": "error",
+        "prefer-const": "error",
+        "no-constant-condition": ["error", {
+            checkLoops: "all"
+        }]
+    },
+}, {
+    files: ["**/*.ts", "**/*.mts", "**/*.cts", "**/*.tsx"],
+
+    rules: {
+        "no-undef": "off",
+    },
+}]);
