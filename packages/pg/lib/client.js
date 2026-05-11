3bb9fbaa5f1b25078cd4ba12d501d4bb05677d9a
sebastian krämer <sk@dotob.de>
2026-05-11 22:43:46 +0200

    Add error handling for non-function callback (#3561)
    * Add error handling for non-function callback
    
    catch callback not a function earlier to get a proper callstack. later when executing the callback the stack may be wrong/insufficient.
    
    * fix: lint
    
    * fix: lint
    
    * fix: test
    
    * feat: add test for new error

--- packages/pg/lib/client.js
+++ packages/pg/lib/client.js
@@ -633,6 +633,10 @@ class Client extends EventEmitter {
           Error.captureStackTrace(err)
           throw err
         })
+      } else {
+        if (!(typeof query.callback === 'function')) {
+          throw new Error('callback is not a function')
+        }
       }
     }
 
